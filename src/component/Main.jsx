5b0b3510dc204b569320d3f1b03eeb3afaeb8483
mubby101 <muuh4mub@gmail.com>
2021-01-29 12:25:12 +0100

    update data base

---
+++ src/component/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+return(
+  <div>
+    <div className="main1">
+      <p>We are Good Space     <span className='span'>Super awesome creative agency London based</span></p>
+      <button>Contact Us</button>
+    </div>
+     
+  </div>
+)
+}
+
+export default Main
