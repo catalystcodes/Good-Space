2fb51fb021b7db555e38f08b1cc54a6c0078232c
mubby101 <muh4mub@gmail.com>
2021-01-29 13:19:37 +0100

    update data base

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -7,6 +7,37 @@ return(
       <p>We are Good Space     <span className='span'>Super awesome creative agency London based</span></p>
       <button>Contact Us</button>
     </div>
+
+    <div className="main2">
+      <div className="sub1-main2">
+      <div className='div1'>
+      <img src="/Images/seaker.png" alt=""/>
+      <p>Very Flexible</p>
+      </div>
+
+      <div className='div2'>
+      <img src="/Images/seaker.png" alt=""/>
+      <p>Very Flexible</p>
+      </div>
+
+      <div className='div3'>
+      <img src="/Images/seaker.png" alt=""/>
+      <p>Very Flexible</p>
+      </div>    
+    </div>
+      <div className="div4">
+        <p>Vestibulum id ligula porta felis euismod semper. ali d dolor id nibh ultricies vehicula ut id elit. Nulla vitae libero, a pharetra augue. Magna Etiam</p>
+        <a href="#1">Learn More</a>
+      </div>
+      <div className="div5">
+      <p>Vestibulum id ligula porta felis euismod semper. ali d dolor id nibh ultricies vehicula ut id elit. Nulla vitae libero, a pharetra augue. Magna Etiam</p>
+        <a href="#1">Learn More</a>
+      </div>
+      <div className="div6">
+      <p>Vestibulum id ligula porta felis euismod semper. ali d dolor id nibh ultricies vehicula ut id elit. Nulla vitae libero, a pharetra augue. Magna Etiam</p>
+        <a href="#1">Learn More</a>
+      </div>
+  </div>
      
   </div>
 )
