4c6f992980149e6e0915fd29cd797c9529852b2c
muhammad <muh4mub@gmail.com>
2022-05-16 16:29:56 +0100

    update database

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -12,94 +12,94 @@ return(
       </div>
     <div className='sub-nav'>
       <img src="/Images/defaultlogo.png" alt="mubb"/>
-        <ul className="sub1-nav">
-          <li><a href="/">Home</a></li>
-          <li><a href="/">Portfolio</a> 
+        <ul>
+          <li className= "active"><a href="#">Home</a></li>
+          <li><a href="#">Portfolio</a> 
             <ul>
-              <li>Portfolio 2 Colomns</li>
-              <li>Portfolio 3 Colomns</li>
-              <li>Portfolio 4 Colomns</li>
-              <li>Portfolio with Right Sidebar
+              <li><a href="#">Portfolio 2 Colomns</a></li>
+              <li><a href="#">Portfolio 3 Colomns</a></li>
+              <li><a href="#">Portfolio 4 Colomns</a></li>
+              <li><a href="#">Portfolio with Right Sidebar</a>
                 <ul>
-                  <li>2 Colomns right sidebar</li>
-                  <li>3 Colomns right sidebar</li>
-                  <li>3 Colomns right sidebar</li>
+                  <li><a href="#">2 Colomns right sidebar</a></li>
+                  <li><a href="#">3 Colomns right sidebar</a></li>
+                  <li><a href="#">4 Colomns right sidebar</a></li>
                 </ul>
               </li>
-              <li>Portfolio with Left Sidebar
+              <li><a href="#">Portfolio with Left Sidebar</a>
                 <ul>
-                 <li>2 Colomns left sidebar</li>
-                 <li>3 Colomns left sidebar</li>
-                 <li>4 Colomns left sidebar</li>
+                 <li><a href="#">2 Colomns left sidebar</a></li>
+                 <li><a href="#">3 Colomns left sidebar</a></li>
+                 <li><a href="#">4 Colomns left sidebar</a></li>
                 </ul> 
               </li>
-              <li>Portfolio with both sidebar</li>
-              <li>Portfolio with filterer
+              <li><a href="#">Portfolio with both sidebar</a></li>
+              <li><a href="#">Portfolio with filterer</a>
                 <ul>
-                  <li>2 Colomns with filterer</li>
-                  <li>3 Colomns with filterer</li>
-                  <li>4 Colomns with filterer</li>
+                  <li><a href="#">2 Colomns with filterer</a></li>
+                  <li><a href="#">3 Colomns with filterer</a></li>
+                  <li><a href="#">4 Colomns with filterer</a></li>
                 </ul>
               </li>
-              <li>Potfolio Gallery Style
+              <li><a href="#">Potfolio Gallery Style</a>
                 <ul>
-                  <li>2 Colomns Gallery Style</li>
-                  <li>3 Colomns Gallery</li>
-                  <li>4 Colomns Gallery</li>
+                  <li><a href="#">2 Colomns Gallery Style</a></li>
+                  <li><a href="#">3 Colomns Gallery Style</a></li>
+                  <li><a href="#">4 Colomns Gallery Style</a></li>
                 </ul>
               </li>
-              <li>Gallery
+              <li><a href="#">Gallery</a>
                 <ul>
-                  <li>Gallery 2 Colomns</li>
-                  <li>Gallery 3 Colomns</li>
-                  <li>Gallery 4 Colomns</li>
+                  <li><a href="#">Gallery 2 Colomns</a></li>
+                  <li><a href="#">Gallery 3 Colomns</a></li>
+                  <li><a href="#">Gallery 4 Colomns</a></li>
                 </ul>
               </li>
             </ul>
           </li>
 
-          <li><a href="/">Blog</a>
+          <li><a href="#">Blog</a>
             <ul>
-              <li>Blog with Right Sidebar</li>
-              <li>Blog with Left Sidebar</li>
-              <li>Blog with Both Sidebar</li>
-              <li>Blog without sidebar</li>
+              <li><a href="#">Blog with Right Sidebar</a></li>
+              <li><a href="#">Blog with Left Sidebar</a></li>
+              <li><a href="#">Blog with Both Sidebar</a></li>
+              <li><a href="#">Blog without sidebar</a></li>
             </ul>
           </li>
 
-          <li><a href="/">Shortcodes</a>
+          <li><a href="#">Shortcodes</a>
             <ul>
-              <li>Typography</li>
-              <li>Accordion</li>
-              <li>Block Quote</li>
-              <li>Buttons</li>
-              <li>Colomns</li>
-              <li>Divider</li>
-              <li>Dropcap</li>
-              <li>Gallery</li>
-              <li>Light Box & Frame</li>
-              <li>List Style</li>
-              <li>Message Box</li>
-              <li>Price Table</li>
-              <li>Social Icons</li>
-              <li>Tabs</li>
-              <li>Testimonial</li>
-              <li>Toggle Boxes</li>
-              <li>Video</li>
+              <li><a href="#">Typography</a></li>
+              <li><a href="#">Accordion</a></li>
+              <li><a href="#">Block Quote</a></li>
+              <li><a href="#">Buttons</a></li>
+              <li><a href="#">Colomns</a></li>
+              <li><a href="#">Divider</a></li>
+              <li><a href="#">Dropcap</a></li>
+              <li><a href="#">Gallery</a></li>
+              <li><a href="#">Light Box & Frame</a></li>
+              <li><a href="#">List Style</a></li>
+              <li><a href="#">Message Box</a></li>
+              <li><a href="#">Price Table</a></li>
+              <li><a href="#">Social Icons</a></li>
+              <li><a href="#">Tabs</a></li>
+              <li><a href="#">Testimonial</a></li>
+              <li><a href="#">Toggle Boxes</a></li>
+              <li><a href="#">Video</a></li>
             </ul>
           </li>
 
-          <li><a href="/">Features</a>
+          <li><a href="#">Features</a>
             <ul>
-              <li>Page Builder</li>
-              <li>Thumbnail Management</li>
-              <li>Creating Gallery</li>
-              <li>Creating Blog</li>
-              <li>Creating Portfolio</li>
+              <li><a href="#">Page Builder</a></li>
+              <li><a href="#">Thumbnail Management</a></li>
+              <li><a href="#">Creating Gallery</a></li>
+              <li><a href="#">Creating Blog</a></li>
+              <li><a href="#">Creating Portfolio</a></li>
             </ul>
           </li>
 
-          <li><a href="/">Contact</a></li>
+          <li><a href="#">Contact</a></li>
         </ul>
       
     </div>
